2a46674c94291886fae978dc9946583092de8254
Armand <armand@metmetric.co.za>
2025-07-24 12:06:31 +0200

    Updated name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="mm_coms_api",
+    name="mmComsClient",
     version="0.1.0",
     author="Reuben",
     author_email="reuben@metmetric.co.za",
